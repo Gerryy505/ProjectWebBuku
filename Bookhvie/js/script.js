4d4a95d44b70ed2e6eedc338f6e40640ffcbd09d
Gerryy505 <syahrezagerry4@gmail.com>
2026-05-06 07:57:10 +0700

    first commit

---
+++ Bookhvie/js/script.js
@@ -0,0 +1,15 @@
+function login() {
+  window.location.href = "home.html";
+}
+
+function goHome() {
+  window.location.href = "home.html";
+}
+
+function goLibrary() {
+  window.location.href = "library.html";
+}
+
+function goDetail() {
+  window.location.href = "detail.html";
+}
